cda4daef40f21b626edf2803f31e1b33a918c3bd
Thomas Ardal <thomasardal@gmail.com>
2024-11-15 14:16:43 +0100

    .NET 9 sample

---
+++ samples/Elmah.Io.Client.Extensions.Correlation.AspNetCore90/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Elmah.Io.Client.Extensions.Correlation.AspNetCore90.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            _logger.LogWarning("An error is about to happen");
+            throw new ApplicationException("The error happens");
+        }
+    }
+}
